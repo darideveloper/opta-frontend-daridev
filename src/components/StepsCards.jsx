6a49f5a58d869a567e1dcef8c2af5c6f413430bf
daridev <darideveloper@gmail.com>
2025-02-12 10:22:15 -0600

    feat (StepsCards): show more button logic

--- src/components/StepsCards.jsx
+++ src/components/StepsCards.jsx
@@ -1,6 +1,9 @@
 // Icons
 import { CircleChevronDown } from "lucide-react"
 
+// Libs
+import { useState } from "react"
+
 /**
  * StepsCard component (render a list of steps with "more" button)
  * 
@@ -11,20 +14,38 @@ import { CircleChevronDown } from "lucide-react"
  */
 export default function StepsCards({ steps }) {
 
+  const [visibleSteps, setVisibleSteps] = useState(1)
+
+  const visibleStepsList = steps.slice(0, visibleSteps)
+
   return (
     <>
       {
-        steps.map(({ title, content }, index) => (
-          <div key={index} className="mb-4 p-4 rounded-lg bg-white shadow">
-            <div className="font-medium text-[#7D3C98] mb-2">{title}</div>
-            <p>{content}</p>
-            <div className="flex space-x-2 mt-2">
-              <button className="text-[#7D3C98] hover:text-purple-800">
-                <CircleChevronDown className="w-5 h-5" />
-              </button>
+        visibleStepsList.map(({ title, content }, index) => {
+
+          const isLastStep = index === visibleStepsList.length - 1
+          const moreSteps = steps.length - visibleSteps
+
+          return (
+            <div key={index} className="mb-4 p-4 rounded-lg bg-white shadow">
+              <div className="font-medium text-[#7D3C98] mb-2">{title}</div>
+              <p>{content}</p>
+              <div className="flex space-x-2 mt-2">
+                <button
+                  className={`
+                    text-[#7D3C98] 
+                    hover:text-purple-800
+                    ${!isLastStep && 'opacity-50'}
+                  `}
+                  onClick={() => setVisibleSteps(visibleSteps + 1)}
+                  disabled={!isLastStep}
+                >
+                  <CircleChevronDown className="w-5 h-5" />
+                </button>
+              </div>
             </div>
-          </div>
-        ))
+          )
+        })
       }
     </>
   )
