2004d05faa3995303e6c67931bde2541b18755f7
daridev <darideveloper@gmail.com>
2025-03-10 16:48:45 -0600

    fix (App): remove react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,13 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+// import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 // import { AuthProvider } from './contexts/AuthContext';
 //import PrivateRoute from './components/PrivateRoute';
 //import Login from './pages/Login';
 import Chat from './pages/Chat';
-import AdminPanel from './pages/AdminPanel';
+// import AdminPanel from './pages/AdminPanel';
 
 function App() {
   return (
-    // <AuthProvider>
-      <Router>
-        <Routes>
-          {/* <Route path="/login" element={<Login />} /> */}
-          <Route
-            path="/chat"
-            element={
-
-              <Chat />
-
-            }
-          />
-          <Route
-            path="/admin/*"
-            element={
-
-              <AdminPanel />
-
-            }
-          />
-          <Route path="/" element={<Navigate to="/chat" replace />} />
-        </Routes>
-      </Router>
-    // </AuthProvider>
+    <Chat />
   );
 }
 
