3e2e6645792fbb90fddade04ea2e84d7818c5214
daridev <darideveloper@gmail.com>
2025-03-18 16:49:03 -0600

    feat (App): show chat or login screen based in token value

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import Chat from './pages/Chat';
-import Login from './pages/Login';
+import Chat from './pages/Chat'
+import Login from './pages/Login'
+
+// Zustand
+import { useAuthStore } from '../stores/auth'
 
 function App() {
+
+  const token = useAuthStore(state => state.token)
+
   return (
     <>
-      {/* <Chat /> */}
-      <Login />
+      {
+        token
+          ? <Chat />
+          : <Login />
+      }
     </>
-  );
+  )
 }
 
-export default App;
+export default App
