02e2eb02f9e663b1a1439949ca9d9b673e86bbeb
daridev <darideveloper@gmail.com>
2025-03-20 16:16:33 -0600

    feat (store/auth): save token in session storage

--- stores/auth.js
+++ stores/auth.js
@@ -1,7 +1,10 @@
 import { create } from 'zustand'
 
 export const useAuthStore = create((set) => ({
-  token: "",
-  setToken: (token) => set({ token }),
+  token: sessionStorage.getItem("token", ""),
+  setToken: (token) => { 
+    sessionStorage.setItem("token", token)
+    set({ token })
+  },
   deleteToken: () => set({ token: "" }),
 }))
